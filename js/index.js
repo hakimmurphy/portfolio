d8b2983d7a015592989f716922bd6d60b9b9fef9
Hakim Murphy <machiningdreams@gmail.com>
2023-11-13 13:06:01 +0100

    add a skill

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 const navToggle = document.querySelector('.nav-toggle')
 const navLinks = document.querySelectorAll('.nav__link')
 const progress = document.querySelector('.progress-bars-wrapper')
-const progressBarPrecents = [95, 85, 80, 75, 80, 85, 70, 65, 75, 78, 85, 70, 85, 90]
+const progressBarPrecents = [95, 85, 80, 75, 80, 85, 70, 65, 75, 78, 85, 70, 85, 90, 80]
 
 window.addEventListener('scroll', () => {
     mainFn()
